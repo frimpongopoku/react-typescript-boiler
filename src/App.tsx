1987dd609fef2f46b88fe831bea82fdaa0478f39
Frimpong <pongofrimi@gmail.com>
2023-05-08 22:28:34 +0400

    number

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [number, setNumber] = useState("")
+  const [number, setNumber] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
